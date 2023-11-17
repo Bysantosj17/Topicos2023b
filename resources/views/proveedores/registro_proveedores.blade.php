c0e17633504019807fd5b204b7458dfd90867152
Bysantosj17 <uriel_barbosasan@hotmail.com>
2023-11-17 00:45:07 -0600

    17/11/23 12:45

--- resources/views/proveedores/registro_proveedores.blade.php
+++ resources/views/proveedores/registro_proveedores.blade.php
@@ -1,1 +1,57 @@
-<h1>Registro Proveedor</h1>
+@extends('layouts.dashboard')
+@section('title', 'Registro proveedor')
+@section('subtitulo', 'REGISTRO PROVEEDOR')
+@section('content-dashboard')
+<div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+    <h1>Registro proveedor:</h1>
+    <fieldset>
+        <form action="" method="POST">
+
+            @csrf
+
+            <label>
+                Nombres:</br>
+                <input type="text" name="nombres">
+            </label>
+
+            <label>
+                Apellido paterno:</br>
+                <input type="text" name="ap_paterno">
+            </label>
+
+            <label>
+                Apellido materno:</br>
+                <input type="text" name="ap_materno">
+            </label>
+
+            <label>
+                Telefono:</br>
+                <input type="number" name="telefono" min="10">
+            </label>
+            </br>
+            <label>
+                Correo electronico:</br>
+                <input type="email" name="email">
+            </label>
+
+            <label>
+                Direccion:</br>
+                <input type="text" name="direccion">
+            </label>
+
+            <label>
+                Compañia:</br>
+                <input type="text" name="compania">
+            </label>
+            <br>
+            <label>
+                Descripcion:</br>
+                <textarea type="text" name="descripcion">
+                </textarea>
+            </label></br>
+            <button type="submit">Registrar proveedor</button>
+        </form>
+    </fieldset>
+</div>
+@endsection
+
